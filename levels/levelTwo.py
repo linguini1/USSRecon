f7758beb88fd938ad6c5058f1eeb0241f529a8c1
linguini1 <matteo.golin@gmail.com>
2021-01-19 19:30:51 -0500

    Made appending 6 items to inventory more efficient, added in the second inventory addition that I previously forgot, and made Caviar a reward because the battle with two soldiers was very health consuming.

--- levels/levelTwo.py
+++ levels/levelTwo.py
@@ -27,7 +27,7 @@ play_music(file="resources/common/soundFiles/plain.wav")  # Starting outside noi
 weapon_switch(player, guard)  # Switch weapons
 
 # Victory
-for item in [RifleAmmo(), Vodka(), Vodka(), Vodka(), Semechki(), Semechki()]:
+for item in [RifleAmmo(), Caviar(), Vodka(), Vodka(), Semechki(), Semechki()]:
     player.inventory.append(item)  # Collecting loot
 
 text_reader("resources/levelTwo/gate.txt", narrator=True)  # Victory and moving to the base's entrance
@@ -53,6 +53,8 @@ weapon_switch(player, soldierTwo)  # Weapon switch
 
 # Victory
 text_reader("resources/levelTwo/bridge.txt", narrator=True)  # Narration
+player.inventory.append(Semechki())  # Semechki
+player.inventory.append(Semechki())  # Semechki x2
 
 # Clear story
 enter_cont()
